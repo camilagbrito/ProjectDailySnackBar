96960f7b4a4637a291b2ec54b06babc980e6cf8b
Camila Brito <camilagbrito48@gmail.com>
2023-09-20 11:27:22 +0100

    ShoppingCart Completed

--- DailySnacksBar/Models/ShoppingCart.cs
+++ DailySnacksBar/Models/ShoppingCart.cs
@@ -1,4 +1,5 @@
 ﻿using DailySnacksBar.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace DailySnacksBar.Models
 {
@@ -30,6 +31,78 @@ namespace DailySnacksBar.Models
             {
                 ShoppingCartId = shoppingCartId
             };
+        }
+
+        public void AddToShoppingCart(Snack snack)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.SingleOrDefault(x =>
+            x.Snack.Id == snack.Id && x.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem != null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Snack = snack,
+                    Quantity = 1
+                };
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+                    
+            }
+            else
+            {
+                shoppingCartItem.Quantity ++;
+            }
+
+            _context.SaveChanges();
+        }
+
+        public void RemoveToShoppingCart(Snack snack)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(x =>
+            x.Snack.Id == snack.Id && x.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem != null)
+            {
+               if(shoppingCartItem.Quantity > 1)
+                {
+                    shoppingCartItem.Quantity--;
+                }
+                else
+                {
+                    _context.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+
+            }
+            _context.SaveChanges();
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ??
+                (ShoppingCartItems =
+                _context.ShoppingCartItems
+                .Where(x => x.ShoppingCartId == ShoppingCartId)
+                .Include(x => x.Snack)
+                .ToList());
+                
+        }
+
+        public void ClearShoppingCart()
+        {
+            var shoppingCartItems = _context.ShoppingCartItems
+                                    .Where(x => x.ShoppingCartId == ShoppingCartId);
+
+            _context.ShoppingCartItems.RemoveRange(shoppingCartItems);
+            _context.SaveChanges();
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            var total = _context.ShoppingCartItems
+                .Where(x => x.ShoppingCartId == ShoppingCartId)
+                .Select(x => x.Snack.Price * x.Quantity).Sum();
+            return total;
         }
     }
 }
