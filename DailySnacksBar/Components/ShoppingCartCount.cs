ee78c8465b27333060cfab61d475c1fed1508759
Camila Brito <camilagbrito48@gmail.com>
2023-09-21 11:16:29 +0100

    ShoppingCartCount

---
+++ DailySnacksBar/Components/ShoppingCartCount.cs
@@ -0,0 +1,32 @@
+﻿using DailySnacksBar.Models;
+using DailySnacksBar.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailySnacksBar.Components
+{
+    public class ShoppingCartCount: ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartCount(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartVM = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartVM);
+        }
+
+    }
+}
